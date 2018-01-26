738c8efedcbfdbe750ec98b19aee02ff6bb46dc9
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2018-01-26 14:09:19 -0300

    Creating NVQ page

---
+++ page-training-programs-nvq.php
@@ -0,0 +1,109 @@
+<?php $page_name = "training-programs-nvq" ?>
+<?php include('header.php'); ?>
+
+<section class="hero">
+    <div class="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="hero-title">The FastTrack NVQ Diploma</div>
+                    <p class="hero-text">Train your staff for a nationally recognised qualification in as little as three months – without the heavy admin workload.</p>
+                    <a class="btn green">Book a time to chat</a>
+                </div>
+                <div class="col-md-7">
+                    <img class="img-responsive hero-img" src="images/hero-nvq.png" width="545">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p>The new FastTrack NVQ Diploma in Health and Social Care is nothing short of revolutionary. It’s fast, free and effective.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="inner-menu">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <ul class="menu">
+                    <li><a href="#">Who's it for</a></li>
+                    <li><a href="#">Organisational benefits</a></li>
+                    <li><a href="#">How does it work?</a></li>
+                    <li><a href="#">How long is the training?</a></li>
+                    <li><a href="#">How much does it cost?</a></li>
+                    <li><a href="#">Full comparison</a></li>
+                    <li><a href="#">Faqs</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="partners">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="title">Our <b class="purple">partners</b></div>
+            </div>    
+            <div class="col-md-9">
+                 <div class="flexslider partners-slider">
+                    <ul class="slides">
+                      <li class="slide">
+                        <img src="images/partners/partner1.png" class="img-responsive" title="slide" alt="slide">
+                      </li>
+                      <li class="slide">
+                        <img src="images/partners/partner2.png" class="img-responsive" title="slide" alt="slide">
+                      </li>
+                      <li class="slide">
+                        <img src="images/partners/partner3.png" class="img-responsive" title="slide" alt="slide">
+                      </li>
+                      <li class="slide">
+                       <img src="images/partners/partner4.png" class="img-responsive" title="slide" alt="slide">
+                      </li>
+                        <li class="slide">
+                       <img src="images/partners/partner5.png" class="img-responsive" title="slide" alt="slide">
+                      </li>
+                      <li class="slide">
+                        <img src="images/partners/partner6.png" class="img-responsive" title="slide" alt="slide">
+                      </li>
+                      <li class="slide">
+                        <img src="images/partners/partner7.png" class="img-responsive" title="slide" alt="slide">
+                      </li>
+                      <li class="slide">
+                        <img src="images/partners/partner8.png" class="img-responsive" title="slide" alt="slide">
+                      </li>
+                        <li class="slide">
+                        <img src="images/partners/partner9.png" class="img-responsive" title="slide" alt="slide">
+                      </li>
+                        <li class="slide">
+                        <img src="images/partners/partner10.png" class="img-responsive" title="slide" alt="slide">
+                      </li>
+                        <li class="slide">
+                        <img src="images/partners/partner11.png" class="img-responsive" title="slide" alt="slide">
+                      </li>
+    
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="whos-for">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="title center"><b class="green">Who's it for</b></div>
+                <div class="green-box">The Fast Track NVQ Diploma is available to all learners through levels 2 – 5 in Health and Social Care. Courses at each level are designed to equip learners with the skills and knowledge needed to care for others in a broad range of health or social care settings.</div>
+            </div>
+        </div>
+    </div>
+
+</section>
+<?php include('footer.php'); ?>
